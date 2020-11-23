30229bfda154b74b47b4eecceb76f95364fb660c
yosuke <yosuke@cowcowhomes.webcrow.jp>
2020-11-23 13:14:41 +0900

    多言語周りで漏れていた部分があった

--- themes/bizvektor/scripts/helper.js
+++ themes/bizvektor/scripts/helper.js
@@ -18,8 +18,34 @@ hexo.extend.helper.register('get_pages', function () {
   const self = this;
   //現在表示中のページの言語
   const lang = self.get_current_lang(this.page);
+  if(this.site.data.resources[lang] == undefined)
+    return [];
+  else
+    return this.site.data.resources[lang].pages;
+});
 
-  return this.site.pages.filter(function(item){
-    return self.get_current_lang(item) == lang && item.layout != "index";
+hexo.extend.helper.register('gen_lang_switches', function(label){
+  const self = this;
+  return self.get_langs().map(function(lang){
+    const lang_name = self.site.data.resources[lang].lang_name;
+   	if(self.page.layout == undefined || self.page.layout == "index")
+    {
+  		return {url:`/${lang}`, label:lang_name};
+    }
+  	else
+    {
+  		var tokens = self.page.path.split("/");
+  		var url = "";
+  		var lang_tag_index = 0;
+  		for(var i = 0;i < tokens.length; i++)
+      {
+  			url += "/";
+  			if(i == lang_tag_index)
+  			  url += lang;
+  			else if(tokens[i] != "index.html")	//index.htmlはなくても動く
+  				url += tokens[i];
+      }
+  		return {url:`${url}`, label:lang_name};
+    }
   });
 });
